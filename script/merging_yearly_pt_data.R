72185c7144c005ce2a4225a734ac5ba44c01553c
rickard11 <100870004+rickard11@users.noreply.github.com>
2024-04-15 16:46:26 -0700

    Created new merged files

--- script/merging_yearly_pt_data.R
+++ script/merging_yearly_pt_data.R
@@ -1,8 +1,21 @@
 NEC22<-read.csv("data/EastBridge_PTdata_2018-2022wy.csv")
 NEC23<-read.csv("data/East_Channel_levelLogger_2023.csv")
+NEC23<-NEC23[,c(1:3,5:8)]
+NECall<-rbind(NEC22,NEC23)
 
 NPB22<-read.csv("data/PhelpsBridge_PTdata_2018-2022wy.csv")
-NPB23<-read.csv("data/Phelps_Levelogger_2023.csv")
+NPB23<-read.csv("data/Phelps_Levelogger_2023.csv",skip=11)
+NPB23<-NEC23[,c(1,2,4,5)]
+NPB23$CONDUCTIVITY<-NA
+NPB23$WSE_ft<-
 
 NVB22<-read.csv("data/Venoco_PTdata_2018-2022wy.csv")
 NVB23<-read.csv("data/Veneco_Bridge_levelLogger_2023.csv")
+NVB23<-NVB23[,c(1,2,3,5,6,7,8)]
+colnames(NVB23)[6]<-"CONDUCTIVITY"
+NVBall<-rbind(NVB22,NVB23)
+
+
+head(NVB22)
+head(NVB23)
+
