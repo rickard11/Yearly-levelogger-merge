65d6258e9d50a992d0b1e22331b38331197e2524
rickard11 <100870004+rickard11@users.noreply.github.com>
2024-04-15 15:28:58 -0700

    Added datafiles and read in to script.

---
+++ script/merging_yearly_pt_data.R
@@ -0,0 +1,8 @@
+NEC22<-read.csv("data/EastBridge_PTdata_2018-2022wy.csv")
+NEC23<-read.csv("data/East_Channel_levelLogger_2023.csv")
+
+NPB22<-read.csv("data/PhelpsBridge_PTdata_2018-2022wy.csv")
+NPB23<-read.csv("data/Phelps_Levelogger_2023.csv")
+
+NVB22<-read.csv("data/Venoco_PTdata_2018-2022wy.csv")
+NVB23<-read.csv("data/Veneco_Bridge_levelLogger_2023.csv")
