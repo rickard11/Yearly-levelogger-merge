96f943f9c98e4f5cea22735bbab40657a8254f5c
rickard11 <100870004+rickard11@users.noreply.github.com>
2024-04-16 09:24:24 -0700

    finished script

--- script/merging_yearly_pt_data.R
+++ script/merging_yearly_pt_data.R
@@ -2,20 +2,26 @@ NEC22<-read.csv("data/EastBridge_PTdata_2018-2022wy.csv")
 NEC23<-read.csv("data/East_Channel_levelLogger_2023.csv")
 NEC23<-NEC23[,c(1:3,5:8)]
 NECall<-rbind(NEC22,NEC23)
+write.csv(NECall,"data/created-data/EastBridge_PTdata_2018-2023wy.csv")
+
 
 NPB22<-read.csv("data/PhelpsBridge_PTdata_2018-2022wy.csv")
 NPB23<-read.csv("data/Phelps_Levelogger_2023.csv",skip=11)
-NPB23<-NEC23[,c(1,2,4,5)]
+NPB23<-NPB23[,c(1,2,4,5)]
+NPB23$X<-1
 NPB23$CONDUCTIVITY<-NA
-NPB23$WSE_ft<-
+NPB23$WSE_ft<-NPB23$LEVEL+9.99
+NPBall<-rbind(NPB22,NPB23)
+write.csv(NPBall,"data/created-data/PhelpsBridge_PTdata_2018-2023wy.csv")
 
 NVB22<-read.csv("data/Venoco_PTdata_2018-2022wy.csv")
 NVB23<-read.csv("data/Veneco_Bridge_levelLogger_2023.csv")
 NVB23<-NVB23[,c(1,2,3,5,6,7,8)]
 colnames(NVB23)[6]<-"CONDUCTIVITY"
 NVBall<-rbind(NVB22,NVB23)
+write.csv(NVBall,"data/created-data/Veneco_Bridge_PTdata_2018-2023wy.csv")
 
 
-head(NVB22)
-head(NVB23)
+head(NPB22)
+head(NPB23)
 
